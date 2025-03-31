10b5b8fcd85cd62e9d941ebe6c882f1fc044a7f0
minhtrile293 <23127273@student.hcmus.edu.vn>
2025-03-31 19:06:44 +0700

    Speed up

--- user/pgtbltest.c
+++ user/pgtbltest.c
@@ -58,9 +58,11 @@ pgaccess_test()
   buf = malloc(32 * PGSIZE);
   if (pgaccess(buf, 32, &abits) < 0)
     err("pgaccess failed");
+
   buf[PGSIZE * 1] += 1;
   buf[PGSIZE * 2] += 1;
   buf[PGSIZE * 30] += 1;
+
   if (pgaccess(buf, 32, &abits) < 0)
     err("pgaccess failed");
   if (abits != ((1 << 1) | (1 << 2) | (1 << 30)))
@@ -68,3 +70,5 @@ pgaccess_test()
   free(buf);
   printf("pgaccess_test: OK\n");
 }
+
+
